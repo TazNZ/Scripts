697a2e3e53000695e0a8435aea569148289266c0
TazNZ <49272905+TazNZ@users.noreply.github.com>
2023-02-12 19:51:02 +1300

    Update AllowPhotoshopBrushes.sh

--- AllowPhotoshopBrushes.sh
+++ AllowPhotoshopBrushes.sh
@@ -6,9 +6,14 @@
 # Purpose:  This script adds the 777 permissions to the Photoshop applications folder, to help assist with 
 # standard users installing or editing their own Photoshop brushes.
 # Tested within Jamf Pro and Microsoft Intune.
+#
+# With Jamf Pro - Add the version year as parameter 4.
 
 # Update: Adobe Creative Cloud may have resolve this in recent versions...
 
-chmod 777 /Applications/Adobe\ Photoshop\ 2020/Presets
+# Variables
+VersionYear=$4
+
+chmod 777 "/Applications/Adobe Photoshop $VersionYear/Presets"
 
 exit 0
